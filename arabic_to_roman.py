63e8f7c8d58c7fa85961d6d38771546dc54b5b6c
Brian He <brian.he17@gmail.com>
2020-01-28 14:26:19 -0500

    implemented algo with greedy approach

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,9 +1,23 @@
-print('Program Start')
+print('---Program Start---')
 
-def arabic2roman(x):
+def arabic2roman(num):
+
     # Numeral KV pairs
     roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    
+    #Pointer starting at first index
+    i = 0
 
-    return x+1
+    #String Builder
+    ans = ''
+
+    while(num > 0):
+        if(num - value[i] >= 0):
+            ans += roman[i]
+            num = num - value[i]
+        else:
+            i+=1    
+
+    return ans
 
--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,3 @@
 from arabic_to_roman import arabic2roman
 
 def test_answer():
-    assert arabic2roman(3) == 4
-    assert arabic2roman(4) == 5
-    assert arabic2roman(4) == 5
