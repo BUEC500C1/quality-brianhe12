63b666befc9cdc1cfabb67d9600d9d6457b3a95f
Brian He <brian.he17@gmail.com>
2020-01-28 01:08:43 -0500

    created pytest cases and roman numeral KV pairs

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,1 +1,9 @@
-print('Testing')
+print('Program Start')
+
+def arabic2roman(x):
+    # Numeral KV pairs
+    roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+    return x+1
+
